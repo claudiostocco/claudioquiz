063ea165a7c5ffaf30e166520c6b98be09c171a2
claudiostocco <claudiostocco@gmail.com>
2021-01-27 08:22:48 -0300

    Tela do quiz, componente Input e Button.

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
-import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
-import GitHubCorner from '../src/components/GitHubCorner';
-
-import Quiz from './quiz';
+import Input from '../src/components/Input'
+import Button from '../src/components/Button'
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -35,11 +31,6 @@ export default function Home() {
   const [name, setName] = React.useState('');
 
   return (
-    <QuizBackground backgroundImage={db.bg}>
-      <Head>
-        <title>AluraQuiz - Modelo Base</title>
-        <meta property="og:image" content={db.bg}/>
-      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
@@ -53,18 +44,15 @@ export default function Home() {
               console.log('Fazendo uma submissão por meio do react');
             }}
             >
-              <input
-                onChange={function (infosDoEvento) {
-                  console.log(infosDoEvento.target.value);
-                  // State
-                  // name = infosDoEvento.target.value;
-                  setName(infosDoEvento.target.value);
-                }}
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
                 placeholder="Diz ai seu nome"
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
@@ -78,7 +66,5 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/claudiostocco" />
-    </QuizBackground>
   );
 }
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,12 +1,26 @@
 import styled from 'styled-components'
 
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import Footer from '../src/components/Footer';
+import QuizContainer from '../src/components/QuizContainer';
+
 function Quiz() {
-    const H1 = styled.h1`
-        color: ${({theme}) => theme.colors.primary};
-    `
+    const name = window.location.search.split('=')[1]
 
     return (
-        <H1>Teste</H1>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Seja bem vindo ao quiz {name}</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
     )
 }
 
